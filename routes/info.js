5ebea72e04053d81e2de66f4c7841fffb38f1dfa
Tijani femi <developerfemi2020@gmail.com>
2022-10-18 15:14:41 +0100

    origin master

---
+++ routes/info.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+let Info = require('../models/info.model');
+
+router.route('/').get((req , res) => {
+    Info.find()
+    .then(info => res.json (info))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req , res) => {
+    const username = req.body.username ;
+    const password = req.body.password ; 
+    const email = req.body.email ;
+    const date = Date.parse(req.body.date);  
+
+    const newInfo = new Info({
+        username,
+        password,
+        email,
+        date,
+
+    })
+
+    newInfo.save()
+    .then(() => res.json('info added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req , res) => {
+    Info.findById(req.params.id)
+        .then(info => res.json(info))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req , res) => {
+    Info.findByIdAndDelete(req.params.id)
+        .then(() => res.json('info deleted'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req , res) => {
+    Info.findById(req.params.id)
+        .then(info => {
+            info.username = req.body.username ;
+            info.password = req.body.password ;
+            info.email = req.body.email ;
+            info.date = Date.parse(req.body.date);
+
+            info.save()
+                .then(() => res.json('info updated'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require ('express')
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5100 ; 
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri , { 
+    useNewUrlParser: true, useCreateIndex: true 
+});
+const connection = mongoose.connection;
+connection.once('open', () => { console.log("MongoDB database connection sucessful")})
+
+const infoRouter = require('./routes/info');
+const userRouter = require('./routes/users');
+
+app.use('/info' , infoRouter);
+app.use('/users' , userRouter);
+
+app.listen(port, () => { console.log(`server is running on port: ${5100}`)})
